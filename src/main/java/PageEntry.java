07b420f4b0e83fd084c80e5863f8a83be9d137e4
Fazlyev Marsel <fazlyev@mail.ru>
2022-07-07 01:15:12 +0300

    renamed file src/main/java/Server.java -> src/main/java/Main.java

---
+++ src/main/java/PageEntry.java
@@ -0,0 +1,25 @@
+public class PageEntry implements Comparable<PageEntry> {
+    private final String pdfName;
+    private final int page;
+    private final int count;
+
+    public PageEntry(String pdfName, int page, int count) {
+        this.pdfName = pdfName;
+        this.page = page;
+        this.count = count;
+    }
+
+
+    @Override
+    public int compareTo(PageEntry o) {
+        return Integer.compare(count, o.count);
+
+    }
+
+    @Override
+    public String toString() {
+        return "\npdfName " + pdfName +
+                "\npage " + page +
+                "\ncount " + count + "\n*************";
+    }
+}
